282c4981ac1ed9a876bac4f1fadf25426c8cb04a
Ayub786 <ayubabc044@gmail.com>
2019-05-30 05:57:19 -0700

    uo

---
+++ Demo/src/Class.java
@@ -0,0 +1,23 @@
+
+public class Class {
+private String name;
+private int sal;
+public Class(String name, int sal) {
+	super();
+	this.name = name;
+	this.sal = sal;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getSal() {
+	return sal;
+}
+public void setSal(int sal) {
+	this.sal = sal;
+}
+
+}
