b53259c52756df19a14ea5011851ec633903f4af
Ayub786 <ayubabc044@gmail.com>
2019-05-30 05:53:24 -0700

    update

---
+++ Ayub/src/Ajith.java
@@ -0,0 +1,26 @@
+
+public class Ajith {
+	
+	private int Empid;
+	private String empname;
+	private int sal;
+	public int getEmpid() {
+		return Empid;
+	}
+	public void setEmpid(int empid) {
+		Empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+}
